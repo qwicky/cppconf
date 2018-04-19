684d3309558ef9fc999658eed16eeb0bafe7f9e8
Maksim Mentiukov <mmentiukov@stc-spb.ru>
2018-04-19 13:27:13 +0300

    adding conan

---
+++ include/hello/hello.h
@@ -0,0 +1,17 @@
+#ifndef HELLO_HELLO_H
+
+#define HELLO_HELLO_H
+
+
+
+#include <ostream>
+
+
+
+namespace hello {
+
+	std::ostream& greet(std::ostream&);
+
+}
+
+#endif
---
+++ src/hello.cpp
@@ -0,0 +1,9 @@
+#include <hello/hello.h>
+
+
+
+std::ostream& hello::greet(std::ostream& stream) {
+
+	return stream << "Hello, world";
+
+}
